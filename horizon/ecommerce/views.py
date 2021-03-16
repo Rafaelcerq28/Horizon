ecd497ac351de90fdfb6dd1792cb18b0e3574257
rafaelcerq28 <rafaelcerq28@gmail.com>
2021-03-16 17:49:35 -0300

    first

--- horizon/ecommerce/views.py
+++ horizon/ecommerce/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'ecommerce/home.html')
+    return render(request,'ecommerce/index.html')
 # Create your views here.
