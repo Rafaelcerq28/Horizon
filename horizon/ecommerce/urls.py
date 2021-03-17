d3413d74d2d5fa29ca809ac71b4b979f24c75a85
rafaelcerq28 <rafaelcerq28@gmail.com>
2021-03-17 17:13:54 -0300

    pagina de prodduto e carrinho

--- horizon/ecommerce/urls.py
+++ horizon/ecommerce/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('detalhesproduto',views.detalhesproduto),
+    path('cart',views.cart),
 ]
--- horizon/ecommerce/views.py
+++ horizon/ecommerce/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 def home(request):
     return render(request,'ecommerce/index.html')
 # Create your views here.
+
+def detalhesproduto(request):
+    return render(request,'ecommerce/detalhesproduto.html')
+
+def cart(request):
+    return render(request,'ecommerce/cart.html')
